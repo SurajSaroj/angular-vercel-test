6b87c771487251032ddea649ac76a7110fccd239
SurajSaroj <surajsaroj3455@gmail.com>
2020-12-15 13:17:47 +0000

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  config: {
+    api_key: 'blt70c731799f445632',
+    delivery_token: 'cs2cea21ca402bce26d5022735',
+    environment: 'production',
+    region: ''
+  }
+};
